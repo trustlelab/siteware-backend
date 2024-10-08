a1d466c1853100d570cd27f14cd341a53966568b
akirataro <akirataro129@gmail.com>
2024-10-08 16:53:38 +0300

    Update Dockerfile and Readme for deployment

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,23 @@
 import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
-import cors from 'cors'; // Import cors
+import cors from 'cors';
 import swaggerJsDoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
 import path from 'path';
-import authRoutes from './routes/authRoutes'; // Assuming your file is named authRoutes.ts
-import agentRoutes from './routes/AgentRoutes'; // Assuming your file is named AgentRoutes.ts
+import authRoutes from './routes/authRoutes';
+import agentRoutes from './routes/AgentRoutes';
 
 const app = express();
 app.use(bodyParser.json());
+const PORT = process.env.PORT || 8000;
 
 // CORS configuration
 const corsOptions = {
-  origin: 'http://localhost:5000', // Allow only this origin
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Specify allowed methods
-  credentials: true, // Allow credentials (if needed)
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
 };
 
-// Enable CORS with the specified options
 app.use(cors(corsOptions));
 
 console.log('Serving uploads directory from: ', path.join(__dirname, 'uploads'));
@@ -33,32 +33,26 @@ const swaggerOptions = {
     },
     servers: [
       {
-        url: 'http://localhost:8000', // Your server URL
+        url: `http://localhost:${PORT}`,
       },
     ],
   },
-  apis: ['./src/routes/*.ts'], // Path to the API docs (update as needed)
+  apis: ['./src/routes/*.ts'],
 };
 
-// Initialize swagger-jsdoc
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
 
-// Serve Swagger documentation with custom CSS
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs, {
-  customCssUrl: '/swagger-custom.css', // URL to your custom CSS (if available)
+  customCssUrl: '/swagger-custom.css',
 }));
 
-// Use auth routes
-app.use('/auth', authRoutes); // Ensure this path is correct for your routes
-app.use('/agent', agentRoutes); // Ensure this path is correct for your agent routes
+app.use('/auth', authRoutes);
+app.use('/agent', agentRoutes);
 
-// Default route
 app.get('/', (req: Request, res: Response) => {
   res.status(200).json({ message: 'Welcome to the Siteware API!' });
 });
 
-// Start the server
-const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
